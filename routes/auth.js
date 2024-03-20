f270d190b62321b0fdb176da9f14a32191d71b8c
offfahad <mughalfahad544@gmail.com>
2024-03-20 16:27:13 +0500

    controllers-added

--- routes/auth.js
+++ routes/auth.js
@@ -2,16 +2,45 @@ const express = require('express');
 
 const router = express.Router();
 
-router.post('/login', (req, res) => {
-  return res.status(201).json({ name: 'Paul', age: 200 });
-});
+const authController = require('../controllers/auth');
 
-router.post('/register', (req, res) => {});
+const { body } = require('express-validator');
 
-router.post('/forgot-password', (req, res) => {});
+const validateUser = [
+  body('name').not().isEmpty().withMessage('Name is required'),
+  body('email').isEmail().withMessage('Please enter a valid email address'),
+  body('password')
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters')
+    .isStrongPassword()
+    .withMessage(
+      'Password must contain at least one uppercase, one lowercase, and one symbol.'
+    ),
+  body('phone')
+    .isMobilePhone()
+    .withMessage('Please enter a valid phone number'),
+];
 
-router.post('/verify-otp', (req, res) => {});
+const validatePassword = [
+  body('newPassword')
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters')
+    .isStrongPassword()
+    .withMessage(
+      'Password must contain at least one uppercase, one lowercase, and one symbol.'
+    ),
+];
 
-router.post('/reset-password', (req, res) => {});
+router.post('/login', authController.login);
+
+router.post('/register', validateUser, authController.register);
+
+router.get('/verify-token', authController.verifyToken);
+
+router.post('/forgot-password', authController.forgotPassword);
+
+router.post('/verify-otp', authController.verifyPasswordResetOTP);
+
+router.post('/reset-password', validatePassword, authController.resetPassword);
 
 module.exports = router;
