f2e02b720696917f9912a90bc58845aa8364bc14
offfahad <mughalfahad544@gmail.com>
2024-03-20 14:07:05 +0500

    app-setup

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log('Helo')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+const app = express();
+const env = process.env;
+
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+app.use(cors());
+app.options('*', cors());
+
+app.get('/', (req, res) => {
+  return res.status(404).send('Sorry, cant find that');
+});
+app.get('/watch/videos/:id', (request, response) => {
+  return response.json({ videoId: request.params.id });
+});
+
+// Start the server
+// localhost >> 192.168.0.0
+const hostname = env.HOST;
+const port = env.PORT;
+
+// mongoose
+//   .connect(env.MONGODB_CONNECTION_STRING)
+//   .then(() => {
+//     console.log('Connected to Database');
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
